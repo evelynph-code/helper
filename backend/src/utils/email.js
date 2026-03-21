b8df29d48acf32f371cee437ebbcc6ca9cbac3a1
PC <PC@MacBook-Pro-5.local>
2026-03-21 03:20:40 -0500

    Update

--- backend/src/utils/email.js
+++ backend/src/utils/email.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY)
 const sendEmail = async ({ to, subject, html }) => {
   try {
     await resend.emails.send({
-      from: 'helper. <onboarding@resend.dev>',
+      from: 'helper. <evelynph727@gmail.com>',
       to,
       subject,
       html,
